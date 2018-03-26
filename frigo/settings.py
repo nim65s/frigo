2a1cd6215c435ca84c3fc32294b935759cda2f24
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-03-26 18:06:35 +0200

    fix settings

--- frigo/settings.py
+++ frigo/settings.py
@@ -143,4 +143,4 @@ LOGGING = {
 
 AUTHENTICATION_BACKENDS = ['yeouia.backends.YummyEmailOrUsernameInsensitiveAuth']
 DJANGO_TABLES2_TEMPLATE = f'{PROJECT}/tables.html'
-LOGIN_URL = '/'
+LOGIN_REDIRECT_URL = '/'
